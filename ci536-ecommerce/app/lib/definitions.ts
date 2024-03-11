d097afd5b92f2e4e546adc2c87d1a2dfd908a89a
davi <39279817+davisaur@users.noreply.github.com>
2024-03-11 15:33:07 +0000

    Fresh Start, Beginning of new HTML & CSS for Products and Login

--- ci536-ecommerce/app/lib/definitions.ts
+++
@@ -1,9 +0,0 @@
-export type Invoice = {
-    id: string;
-    customer_id: string;
-    amount: number;
-    date: string;
-    // In TypeScript, this is called a string union type.
-    // It means that the "status" property can only be one of the two strings: 'pending' or 'paid'.
-    status: 'pending' | 'paid';
-  };
