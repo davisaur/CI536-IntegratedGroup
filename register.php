d82949c585006cb2bfb0f3c62a404ed30810e854
davi <39279817+davisaur@users.noreply.github.com>
2024-05-16 20:26:10 +0100

    update register page css

--- register.php
+++ register.php
@@ -57,8 +57,8 @@ $conn->close();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="stylesheets/alertbox.css">
     <link rel="stylesheet" href="stylesheets/register.css">
-    <link rel="stylesheet" href="stylesheets/login.css">
     <title>Register - Marketplace</title>
 </head>
 <body>
