386321f503993e8a7251b8e686fdd8a629fcffd8
PavanSahadevan46 <pavansahadevan46@gmail.com>
2024-05-17 10:47:01 +0100

    fixed missing $ on line 27

--- checkout.php
+++ checkout.php
@@ -24,7 +24,7 @@
                     exit();
                 }
             } else {
-                echo $qtyCheck->error;
+                echo $qtyCheck->$error;
             }
         }
     }
